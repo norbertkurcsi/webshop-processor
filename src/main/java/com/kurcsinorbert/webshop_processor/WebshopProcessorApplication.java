1b9e0b3edf153bcff17dd68745621f43bac34ba0
Kurcsi Norbert <kurcsi.norbert@edu.bme.hu>
2024-08-24 11:13:01 +0200

    initial commit

---
+++ src/main/java/com/kurcsinorbert/webshop_processor/WebshopProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.kurcsinorbert.webshop_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurcsinorbert/webshop_processor/WebshopProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurcsinorbert.webshop_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
